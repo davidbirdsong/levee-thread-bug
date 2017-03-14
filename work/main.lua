6125bc4a50e50f60608ad0f7878e5ed65ec36777
david birdsong <david@imgix.com>
2017-03-14 21:42:04 +0000

    clean up

--- work/main.lua
+++
@@ -1,7 +0,0 @@
-local levee = require("levee")
-local hub = levee:Hub()
-hub.thread:spawn(function()
-	require("work.work_funcs").func("bar")
-end)
-hub:sleep(10)
-
--- work/work_funcs.lua
+++
@@ -1,6 +0,0 @@
-
-return {
-	func = function(arg)
-		print(arg)
-	end
-}
